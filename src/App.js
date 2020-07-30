53443e931f09fe39e6a5f22f14f8bbb81bcbcd3d
Noal Brown <noalbrown1@gmail.com>
2020-07-29 19:56:46 -0500

    media queries

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      dropMenuVisible: false
     }
   }
 
+  toggleDropDown = () => {
+    this.setState({
+      dropMenuVisible: !this.state.dropMenuVisible
+    })
+  }
 
   render() {
     return (
@@ -22,7 +27,16 @@ class App extends Component {
             <a href="#">TEAM</a>
             <a href="#">CONTACT</a>
           </ul>
-          <button className="drop-menu">Menu</button>
+          <button className="drop-menu" onClick={this.toggleDropDown}>Menu</button>
+          {this.state.dropMenuVisible ? (
+            <nav className="drop-menu-items">
+              <a href="#">SERVICES</a>
+              <a href="#">PORTFOLIO</a>
+              <a href="#">ABOUT</a>
+              <a href="#">TEAM</a>
+              <a href="#">CONTACT</a>
+            </nav>
+          ) : null}
         </header>
         <section className="section-1">
           <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/header-bg.jpg" alt="Bootstrap" />
